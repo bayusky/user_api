3bde71759a5ae6c8c07da5d0b0dbea951e3d43ef
bayusky <bayusangkaya@gmail.com>
2023-07-17 17:26:44 +0700

    Add files via upload

---
+++ backend/config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+import redis
+import pymongo
+
+load_dotenv()
+
+class ApplicationConfig:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    MONGO_CLIENT = pymongo.MongoClient(os.environ["MONGO_URI"])
+    DB = MONGO_CLIENT['Users']
+    
+    SESSION_TYPE = "redis"
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
